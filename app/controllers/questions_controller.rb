c27c4c63e1e3cf353e8c5d348f0e02e126851f31
Mr. Brown <gitcherrypick.work@gmail.com>
2022-05-02 11:21:57 +0500

    Refactoring Questions and Answers actions. Extracting common actions.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  include QuestionsAnswers
   before_action :set_question!, only: %i[show edit update destroy]
 
   def index
@@ -7,10 +8,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = @question.decorate
-    @answer = @question.answers.build
-    @pagy, @answers = pagy(@question.answers.order(created_at: :desc))
-    @answers = @answers.decorate
+    load_question_answers
   end
 
   def new
