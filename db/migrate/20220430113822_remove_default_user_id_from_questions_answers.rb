e9af7edb0dfbff109ef787ddfdeeef1511f2be49
Mr. Brown <gitcherrypick.work@gmail.com>
2022-05-15 22:40:01 +0500

    Preparing to deploy on heroku.
    Prepare db for production.
    
    Fixed migrations. Bad way.
    
    Added Faker to production env too.

--- db/migrate/20220430113822_remove_default_user_id_from_questions_answers.rb
+++ db/migrate/20220430113822_remove_default_user_id_from_questions_answers.rb
@@ -1,11 +1,11 @@
 class RemoveDefaultUserIdFromQuestionsAnswers < ActiveRecord::Migration[7.0]
   def up
-    change_column_default :questions, :user_id, from: User.first.id, to: nil
-    change_column_default :answers, :user_id, from: User.first.id, to: nil
+    change_column_default :questions, :user_id, from: 1, to: nil
+    change_column_default :answers, :user_id, from: 1, to: nil
   end
 
   def down
-    change_column_default :questions, :user_id, from: nil, to: User.first.id
-    change_column_default :answers, :user_id, from: nil, to: User.first.id
+    change_column_default :questions, :user_id, from: nil, to: 1
+    change_column_default :answers, :user_id, from: nil, to: 1
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,37 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# Creating 30 something questions
-# 30.times do
-#   title = Faker::Hipster.sentence(word_count: 3)
-#   body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
-#
-#   Question.create(title:, body:)
-# end
+if Rails.env.production?
+  # clear db
+  Question.destroy_all
+  User.destroy_all
+  Tag.destroy_all
 
-# Set up avatar for each User
-User.find_each do |user|
-  user.send(:set_gravatar_hash)
-  user.save
-end
+  # Creating 30 something questions
+  30.times do
+    title = Faker::Hipster.sentence(word_count: 3)
+    body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
 
-# Generating random Tags
-30.times do
-  title = Faker::Hipster.word
-  Tag.create(title:)
+    Question.create(title:, body:)
+  end
+
+  # default demo user
+  User.create!(
+    email: 'demo@example.com',
+    name: 'demo',
+    password: 'Qwerty-123',
+    password_confirmation: 'Qwerty-123'
+  )
+
+  # Set up avatar for each User
+  # User.find_each do |user|
+  #   user.send(:set_gravatar_hash)
+  #   user.save
+  # end
+
+  # Generating random Tags
+  30.times do
+    title = Faker::Hipster.word
+    Tag.create(title:)
+  end
 end
